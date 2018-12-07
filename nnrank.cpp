7b6e5b61ce3bd496806816c71fb82d04c488839c
Geophagus96 <33130813+Geophagus96@users.noreply.github.com>
2018-12-07 14:04:48 +0800

    Update nnrank.cpp

--- nnrank.cpp
+++ nnrank.cpp
@@ -7,9 +7,9 @@ using namespace arma;
 
 //[[Rcpp::plugins(cpp11)]]
 //[[Rcpp::depends(RcppArmadillo)]]
+/*Rank all data points according to how many neighbourhoods they are in*/
 //[[Rcpp::export]]
-
-uvec sigpointsampling(const mat&knng, const int &t){
+uvec sigpointsampling(const mat&knng){
   int n = knng.n_rows;
   int k = knng.n_cols;
   uvec nnnums(n,fill::zeros);
@@ -21,4 +21,37 @@ uvec sigpointsampling(const mat&knng, const int &t){
   }
   return nnnums;
   
+}
+
+/*Selecting all initial data points using the NN-rank method*/
+//[[Rcpp::export]]
+uvec initialpointsamp(const mat&knng, const uvec&nnnums, const int &cats){
+  uvec nnnum_id= sort_index(nnnums,1);
+  int n = knng.n_rows;
+  int k = knng.n_cols;
+  int center_num = 0;
+  uvec centers(cats,fill::zeros);
+  centers(0) = nnnum_id(0);
+  center_num = center_num + 1;
+  for (int i = 1; i< n; i++){
+    int temp_id = nnnum_id(i);
+    int new_c = 1;
+    for (int j = 0;j< center_num; j++){
+      for (int l = 0; l<k; l++){
+        if (knng(centers(j),l) == temp_id){
+          new_c = 0;
+          break;
+          break;
+        }
+      }
+    }
+    if (new_c == 1){
+      centers(center_num) = temp_id;
+      center_num = center_num+1;
+    }
+    if (center_num == cats){
+      break;
+    }
+  }
+  return centers;
 }
