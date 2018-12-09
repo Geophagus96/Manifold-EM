b1ac91be347a1b41f049fcdf945b1633e3c45394
Geophagus96 <33130813+Geophagus96@users.noreply.github.com>
2018-12-09 10:19:22 +0800

    Update geodist.cpp

--- geodist.cpp
+++ geodist.cpp
@@ -11,7 +11,21 @@ using namespace std;
 /*Since my Rcpp package is an old version, it only supports cpp11. If you are using the latest Rcpp version please change the cpp plugin to cpp14*/
 //[[Rcpp::depends(RcppArmadillo)]]
 //[[Rcpp::export]]
-
+/*
+ The function for calculating geodesic distances when there are more than one connected components. The first 
+ parameter is the shortest path matrix, the second parameter is the euclidean distance matrix and the third 
+ parameter is the distance matrix between different connected components.
+ 
+ The distance between two connected components i and j is defined as such:
+                                      d(i,j) = min d(ci, cj), ci in i, cj in j
+ 
+ Therefore the fourth parameter is the connected components representatives matrix, whose row number and column
+ number is the same as the number of connected components. The (i,j) th entry of which is the node of the i th 
+ component that is closest to the j th connected component, namely the ci chosen for the definition of the 
+ distance d(i,j).
+ 
+ The last parameter is a vector indicating which connected component each node is in.
+ */
 mat geodist(const mat & pathdist, const mat &l2dist, const mat & ccdist, const mat & ccrep, const uvec & ccs){
   mat geodist = pathdist;
   int n = pathdist.n_rows;
